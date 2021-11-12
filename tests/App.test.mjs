87d5e22cf8fd5bf3d9f284e03059943b2020588a
David Geddes <dave@luckypieconepro.lan>
2021-11-12 12:16:42 -0700

    add before hooks

--- tests/App.test.mjs
+++ tests/App.test.mjs
@@ -1,6 +1,18 @@
 import { test } from "uvu";
 import * as assert from 'uvu/assert';
 
+test.before(() => {
+  // runs before all tests
+});
+
+test.before.each(() => {
+  // runs before each test
+})
+
+test.after(() => {
+  // runs after all the tests
+});
+
 test("test", () => {
   assert.is(true, true);
 });
